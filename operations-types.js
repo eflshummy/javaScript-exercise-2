feda554d0e21cc86c98c1afb3f55480eaedf1425
Shumeez Van schalkwyk <vanschalkwykshumeez@gmail.com>
2025-06-02 12:27:48 +0200

    Add files via upload

---
+++ operations-types.js
@@ -0,0 +1,14 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 5; 
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 10;
+// TODO: Convert number1 to a string and store it in a new variable
+let number1string = number1.toString();
+// TODO: Add number1 and number2 and store the result in a variable called sum
+let sum = number1 + number2;
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = number1 * number2;
+// TODO: Print all results to the console
+console.log(`Sum:  ${sum}`);
+console.log(`Product:  ${product}`);
+console.log(`Number1 as string:  ${number1string}`);
